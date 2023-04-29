3c57899bb17a3377c2666e1dcc3bb040a9250424
Nursultan Jangarashev <njangarashev@yandex.kz>
2023-04-29 12:00:17 +0600

    added tasks from exceptions

--- projects/array-objects/index.js
+++ projects/array-objects/index.js
@@ -9,7 +9,20 @@
  Пример:
    forEach([1, 2, 3], (el) => console.log(el)); // выведет каждый элемент массива
  */
-function forEach() {}
+   function forEach(array, fn) {
+    for (let i = 0; i < array.length; i++) 
+  
+    fn(array[i], i, array)};
+  
+  
+  forEach([1, 2, 3], (el) => console.log(el))
+
+    //    function forEach(array, fn) {
+   //       for (const [ix, el] of array.entries()) {
+  //          fn(el, ix, array);
+ //         }
+//         }
+
 
 /*
  Задание 2:
@@ -21,7 +34,19 @@ function forEach() {}
    const newArray = map([1, 2, 3], (el) => el ** 2);
    console.log(newArray); // выведет [1, 4, 9]
  */
-function map() {}
+   function map(array, fn) {
+    const newArray = [];
+  
+    for (const [i, el] of array.entries()) {
+      newArray.push(fn(el, i, array));
+    };
+  
+    return newArray;
+  }
+
+  // точно так же как и в разборе заданий
+
+
 
 /*
  Задание 3:
@@ -33,7 +58,22 @@ function map() {}
    const sum = reduce([1, 2, 3], (all, current) => all + current);
    console.log(sum); // выведет 6
  */
-function reduce() {}
+   function reduce(array, fn, initial) {
+    let startIndex = 0;
+    let all = initial;
+  
+    if (initial === undefined) {
+      startIndex = 1;
+      all = array[0];
+    }
+  
+    for (let i = startIndex; i < array.length; i++) {
+      all = fn(all, array[i], i, array);
+    }
+    return all;
+  }
+
+  // самому не получилось сделать
 
 /*
  Задание 4:
@@ -44,6 +84,8 @@ function reduce() {}
    const keys = upperProps({ name: 'Сергей', lastName: 'Петров' });
    console.log(keys) // выведет ['NAME', 'LASTNAME']
  */
-function upperProps() {}
+   function upperProps(obj) {
+    return Object.keys(obj).map(k => k.toUpperCase());
+  }
 
 export { forEach, map, reduce, upperProps };
