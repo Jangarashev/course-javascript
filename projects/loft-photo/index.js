caf0522725e3af330a7bbd30467b744993ad2b45
Nursultan Jangarashev <njangarashev@yandex.kz>
2023-05-04 14:28:05 +0600

    add some words

--- projects/loft-photo/index.js
+++ projects/loft-photo/index.js
@@ -4,4 +4,5 @@ import('./styles.css');
 const pageNames = ['login', 'main', 'profile'];
 
 document.addEventListener('click', () => {
+    const random = Math.floor(Math.random() * pageNames.length);
 });
